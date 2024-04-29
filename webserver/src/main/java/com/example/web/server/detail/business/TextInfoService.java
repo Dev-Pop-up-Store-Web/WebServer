d6246e355c69258ac0d773b09fd1b5b586a8fe5e
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-04-29 20:01:57 +0900

    refactor: textInfo 메소드 적용

--- webserver/src/main/java/com/example/web/server/detail/business/TextInfoService.java
+++ webserver/src/main/java/com/example/web/server/detail/business/TextInfoService.java
@@ -1,10 +1,8 @@
 package com.example.web.server.detail.business;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 
-import com.data.domain.PopUpStoreInfo;
+import com.example.web.server.detail.domain.info.DetailInfo;
 import com.example.web.server.detail.repository.DetailRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 public class TextInfoService {
 	private final DetailRepository detailRepository;
 
-	public Optional<PopUpStoreInfo> getTextInfo(String id) {
-		return detailRepository.findById(id);
+	public DetailInfo getDetailTextInfo(String id) {
+		return detailRepository.getDetailInfoById(id);
 	}
 }
