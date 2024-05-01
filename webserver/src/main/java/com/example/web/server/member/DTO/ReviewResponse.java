345a14fd72b086bcebb092503c21e6a55d8959eb
34suuuuu <psh7492@gmail.com>
2024-05-01 19:38:37 +0900

    feat: review DTO 추가

---
+++ webserver/src/main/java/com/example/web/server/member/DTO/ReviewResponse.java
@@ -0,0 +1,34 @@
+package com.example.web.server.member.DTO;
+
+import com.example.web.server.member.domain.Review;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewResponse {
+
+	private Long id;
+
+	private int rating;
+
+	private String account;
+
+	private String contents;
+
+	private String popupId;
+
+	public ReviewResponse(Review review) {
+		this.id = review.getId();
+		this.account = review.getAccount();
+		this.contents = review.getContents();
+		this.rating = review.getRating();
+	}
+
+
+}
